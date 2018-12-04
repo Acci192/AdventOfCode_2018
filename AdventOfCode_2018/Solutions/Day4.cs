4964fb1f6d9de416c9e2baeb57e32f6cefe05eda
Acci192 <k.andreas@live.se>
2018-12-04 21:50:56 +0100

    Day4 cleaner solution

--- AdventOfCode_2018/Solutions/Day4.cs
+++ AdventOfCode_2018/Solutions/Day4.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace AdventOfCode_2018.Solutions
 {
@@ -14,113 +15,47 @@ namespace AdventOfCode_2018.Solutions
             var currentId = -1;
             var beginMin = 0;
 
-            var instructions = new SortedDictionary<DateTime, string>();
-            var workers = new Dictionary<int, int>();
+            var workers = new Dictionary<int, int[]>();
             var rows = input.Replace("\r", "").Split('\n');
+
+            Array.Sort(rows);
+
             foreach (var row in rows)
             {
-                var split = row.Split(']');
-                var time = DateTime.Parse(split[0].Substring(1));
-                int id;
-                var info = "_";
-                var idString = Regex.Match(split[1], @"\d{1,4}").Value;
-                if (int.TryParse(idString, out id))
-                {
-                    info = idString;
-                }
-                else if (split[1].Contains("asleep"))
-                {
-                    info = "a";
-                }
-                else
-                {
-                    info = "w";
-                }
-                instructions.Add(time, info);
-            }
-
-            foreach (var instruction in instructions)
-            {
-                
-                if (int.TryParse(instruction.Value, out var temp))
+                var regex = Regex.Match(row, @"\[(.*)\](.*#(\d*)|.*(wakes|asleep))");
+                var time = DateTime.Parse(regex.Groups[1].Value);
+                if (int.TryParse(regex.Groups[3].Value, out var temp))
                 {
                     currentId = temp;
-                    beginMin = 0;
-                    if (instruction.Key.Hour == 12)
-                    {
-                        beginMin = instruction.Key.Minute;
-                    }
-                    
-                } 
-                else if (instruction.Value.Equals("a"))
-                {
-                    beginMin = instruction.Key.Minute;
-                }
-                else
-                {
                     if (!workers.ContainsKey(currentId))
                     {
-                        workers[currentId] = instruction.Key.Minute - beginMin;
-                    }
-                    else
-                    {
-                        workers[currentId] += instruction.Key.Minute - beginMin;
+                        workers[currentId] = new int[60];
                     }
                 }
-            }
-
-            var lazy = workers.FirstOrDefault(x => x.Value == workers.Max(y => y.Value));
-
-            var LazyInstructions = new SortedDictionary<DateTime, string>();
-            var minArray = new int[60];
-            foreach (var instruction in instructions)
-            {
-                if (int.TryParse(instruction.Value, out var temp))
+                else if (regex.Groups[4].Value.Equals("asleep"))
                 {
-                    currentId = temp;
-                    beginMin = 0;
-                    if (temp == lazy.Key)
-                    {
-                        
-                        LazyInstructions.Add(instruction.Key, instruction.Value);
-                    }
-                    
-                }
-                else if (instruction.Value.Equals("a"))
-                {
-                    if (currentId == lazy.Key)
-                    {
-                        beginMin = instruction.Key.Minute;
-                        LazyInstructions.Add(instruction.Key, instruction.Value);
-                    }
+                    beginMin = time.Minute;
                 }
                 else
                 {
-                    if (currentId == lazy.Key)
-                    {
-                        var tempz = instruction.Key.Minute - beginMin;
-                        for (int i = beginMin; i < instruction.Key.Minute; i++)
-                        {
-                            minArray[i]++;
-                        }
-                        LazyInstructions.Add(instruction.Key, instruction.Value);
-                    }
-                        
+                    for (var i = beginMin; i < time.Minute; i++)
+                        workers[currentId][i]++;
                 }
-                
             }
 
-            var max = int.MinValue;
-            var mixIndex = -1;
-            for (int i = 0; i < minArray.Length; i++)
+            var lazyWorker = workers.FirstOrDefault(x => x.Value.Sum() == workers.Max(y => y.Value.Sum()));
+            var maxIndex = 0;
+            var max = lazyWorker.Value[maxIndex];
+            for (var i = 1; i < lazyWorker.Value.Length; i++)
             {
-                if (minArray[i] > max)
+                if (lazyWorker.Value[i] > max)
                 {
-                    max = minArray[i];
-                    mixIndex = i;
+                    max = lazyWorker.Value[i];
+                    maxIndex = i;
                 }
             }
-            return $"{mixIndex * lazy.Key}";
+
+            return $"{maxIndex * lazyWorker.Key}";
         }
 
         public static string B(string input)
@@ -128,59 +63,31 @@ namespace AdventOfCode_2018.Solutions
             var currentId = -1;
             var beginMin = 0;
 
-            var instructions = new SortedDictionary<DateTime, string>();
             var workers = new Dictionary<int, int[]>();
             var rows = input.Replace("\r", "").Split('\n');
+
+            Array.Sort(rows);
+
             foreach (var row in rows)
             {
-                var split = row.Split(']');
-                var time = DateTime.Parse(split[0].Substring(1));
-                int id;
-                var info = "_";
-                var idString = Regex.Match(split[1], @"\d{1,4}").Value;
-                if (int.TryParse(idString, out id))
-                {
-                    info = idString;
-                }
-                else if (split[1].Contains("asleep"))
-                {
-                    info = "a";
-                }
-                else
-                {
-                    info = "w";
-                }
-                instructions.Add(time, info);
-            }
-
-            foreach (var instruction in instructions)
-            {
-
-                if (int.TryParse(instruction.Value, out var temp))
+                var regex = Regex.Match(row, @"\[(.*)\](.*#(\d*)|.*(wakes|asleep))");
+                var time = DateTime.Parse(regex.Groups[1].Value);
+                if (int.TryParse(regex.Groups[3].Value, out var temp))
                 {
                     currentId = temp;
-                    beginMin = 0;
-                    if (instruction.Key.Hour == 12)
-                    {
-                        beginMin = instruction.Key.Minute;
-                    }
-
-                }
-                else if (instruction.Value.Equals("a"))
-                {
-                    beginMin = instruction.Key.Minute;
-                }
-                else
-                {
                     if (!workers.ContainsKey(currentId))
                     {
                         workers[currentId] = new int[60];
                     }
-
-                    for (int i = beginMin; i < instruction.Key.Minute; i++)
-                    {
+                }
+                else if (regex.Groups[4].Value.Equals("asleep"))
+                {
+                    beginMin = time.Minute;
+                }
+                else
+                {
+                    for (var i = beginMin; i < time.Minute; i++)
                         workers[currentId][i]++;
-                    }
                 }
             }
 
