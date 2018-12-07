62b766f57f48eaff45c4ce5dd3c6e67bd813a498
Acci192 <k.andreas@live.se>
2018-12-07 16:07:16 +0100

    Day 7

--- AdventOfCode_2018/Solutions/Day7.cs
+++ AdventOfCode_2018/Solutions/Day7.cs
@@ -2,66 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode_2018.Solutions
 {
     public class Day7
     {
-        private class Node
-        {
-            public char Value { get; set; }
-            public List<char> Connections { get; set; }
-
-            public Node(char value)
-            {
-                Value = value;
-                Connections = new List<char>();
-            }
-        }
         public static string A(string input)
         {
             var rows = input.Replace("\r", "").Split('\n').ToList();
-            var dic = new Dictionary<char, List<char>>();
-            List<Node> graph = new List<Node>();
-            for(char c = 'A'; c <= 'Z'; c++)
+            var graph = new Dictionary<char, List<char>>();
+            // Create each node
+            for (char c = 'A'; c <= 'Z'; c++)
             {
-                dic[c] = new List<char>();
+                graph[c] = new List<char>();
             }
+
+            // Add edges between connected nodes
             foreach (var row in rows)
             {
                 var tempRow = row.Split(' ');
                 var first = tempRow[1].FirstOrDefault();
                 var second = tempRow[7].FirstOrDefault();
 
-                dic[first].Add(second);
-            }
-
-            char startingChar = ',';
-            var starting = new List<char>();
-            for (char c = 'A'; c <= 'Z'; c++)
-            {
-                if(dic.Where(x => x.Value.Contains(c)).Count() == 0)
-                {
-                    startingChar = c;
-                    starting.Add(c);
-
-                }
-            }
-            Console.WriteLine($"***** = {string.Join(",", starting)}");
-
-            foreach (var temp in dic)
-            {
-                Console.WriteLine($"{temp.Key} = {string.Join(",", temp.Value)}");
+                graph[first].Add(second);
             }
 
             var queue = new SortedSet<char>();
             var visited = new HashSet<char>();
-            foreach(var c in starting)
+
+            // Find nodes that doesn't depend on any other node
+            for (char c = 'A'; c <= 'Z'; c++)
             {
-                queue.Add(c);
+                if(graph.Where(x => x.Value.Contains(c)).Count() == 0)
+                {
+                    queue.Add(c);
+                }
             }
-            
+
             var result = new StringBuilder();
             while(queue.Count() != 0)
             {
@@ -69,13 +46,13 @@ namespace AdventOfCode_2018.Solutions
                 visited.Add(current);
                 result.Append(current);
                 queue.Remove(current);
-                foreach(var c in dic[current])
+
+                foreach(var c in graph[current])
                 {
-                    var test = dic.Where(x => !visited.Contains(x.Key)).Select(x => x.Value).ToList();
-                    if (test.Count(x => x.Contains(c)) == 0)
+                    var notVisitedDependency = graph.Where(x => !visited.Contains(x.Key)).Select(x => x.Value).ToList();
+                    if (notVisitedDependency.Count(x => x.Contains(c)) == 0 && !visited.Contains(c))
                     {
-                        if (!visited.Contains(c))
-                            queue.Add(c);
+                        queue.Add(c);
                     }
                     
                 }
@@ -86,76 +63,85 @@ namespace AdventOfCode_2018.Solutions
         public static string B(string input)
         {
             var rows = input.Replace("\r", "").Split('\n').ToList();
-            var dic = new Dictionary<char, List<char>>();
-            List<Node> graph = new List<Node>();
+            var graph = new Dictionary<char, List<char>>();
+            // Create each node
             for (char c = 'A'; c <= 'Z'; c++)
             {
-                dic[c] = new List<char>();
+                graph[c] = new List<char>();
             }
+
+            // Add edges between connected nodes
             foreach (var row in rows)
             {
                 var tempRow = row.Split(' ');
                 var first = tempRow[1].FirstOrDefault();
                 var second = tempRow[7].FirstOrDefault();
 
-                dic[first].Add(second);
+                graph[first].Add(second);
             }
 
             var queue = new SortedSet<char>();
-            for (char c = 'A'; c <= 'Z'; c++)
-            {
-                if (dic.Where(x => x.Value.Contains(c)).Count() == 0)
-                {
-                    queue.Add(c);
-
-                }
-            }
-
-            
-            var workers = new Dictionary<int, int>();
-            for(var i = 0; i < 2; i++)
-            {
-                workers[i] = 0;
-            }
             var visited = new HashSet<char>();
 
-            var time = new Dictionary<char, int>();
+            // Find nodes that doesn't depend on any other node
             for (char c = 'A'; c <= 'Z'; c++)
             {
-                time[c] = 0;
-            }
-
-
-            var result = new StringBuilder();
-            while (queue.Count() != 0)
-            {
-                var current = queue.Min();
-                visited.Add(current);
-
-                //var firstWorker = workers.Min(x => x.Value);
-                //var temp = workers.FirstOrDefault(x => x.Value == firstWorker);
-                //workers[temp.Key] += (current - 64);
-
-                time[current] += 60 +(current - 64);
-                result.Append(current);
-                queue.Remove(current);
-                foreach (var c in dic[current])
+                if (graph.Where(x => x.Value.Contains(c)).Count() == 0)
                 {
-                    var test = dic.Where(x => !visited.Contains(x.Key)).Select(x => x.Value).ToList();
-                    //var max = dic.Where(x => !visited.Contains(x.Key)).Max(x => x.Value).ToList();
-                    if (test.Count(x => x.Contains(c)) == 0)
-                    {
-                        if (!visited.Contains(c))
-                        {
-                            queue.Add(c);
-                            time[c] = time[current];
-                        }
-                            
-                    }
-
+                    queue.Add(c);
                 }
             }
-            return $"{time.Max(x => x.Value)}";
+
+            var completedNodes = new bool[26];
+            var numWorkers = 5;
+            var workers = new List<int>();
+            var workerChar = new List<char>();
+            for(var i = 0; i < numWorkers; i++)
+            {
+                workers.Add(0);
+                workerChar.Add('_');
+            }
+            var result = 0;
+            while(completedNodes.Count(x => x) != 26)
+            {
+                if(workers.Count(x => x > 0) == numWorkers || (queue.Count == 0 && workers.Count(x => x > 0) > 0))
+                {
+                    result++;
+                    for(var i = 0; i < numWorkers; i++)
+                    {
+                        if(workers[i] > 0)
+                        {
+                            workers[i]--;
+                            if(workers[i] == 0)
+                            {
+                                var doneNode = workerChar[i];
+                                completedNodes[doneNode - 65] = true;
+                                visited.Add(doneNode);
+
+                                foreach(var c in graph[doneNode])
+                                {
+                                    var notVisitedDependency = graph.Where(x => !visited.Contains(x.Key)).Select(x => x.Value).ToList();
+                                    if (notVisitedDependency.Count(x => x.Contains(c)) == 0 && !visited.Contains(c))
+                                    {
+                                        queue.Add(c);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    var current = queue.Min();
+
+                    queue.Remove(current);
+                    var freeWorker = workers.FindIndex(x => x == 0);
+                    workers[freeWorker] = 60 + (current - 64);
+                    workerChar[freeWorker] = current;
+                }
+            }
+
+            return $"{result}";
         }
     }
 }
